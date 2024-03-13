d473140bc91b10767f6b6a058874af9b5fbceb6b
Roy Quesada <roy.quesada@gmail.com>
2024-03-13 16:52:13 -0600

    Empty project

---
+++ HatchWorks/HatchWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchWorksApp.swift
+//  HatchWorks
+//
+//  Created by Roy Quesada on 13/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HatchWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HatchWorks/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HatchWorks
+//
+//  Created by Roy Quesada on 13/3/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "e4b81b6" // OMDBAPI
+}
